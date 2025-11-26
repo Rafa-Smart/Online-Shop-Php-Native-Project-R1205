b65a60f412654b4c5859344edecc89972837d785
Rafa Khadafi <rafakhadafi29@gmail.com>
2025-11-26 11:31:11 +0700

    test

--- connect/connection.php
+++ connect/connection.php
@@ -2,6 +2,6 @@
 define("HOST_NAME","localhost");
 define("USER_NAME","root");
 define("PASSWORD","");
-define("DB_NAME","siswa");
+define("DB_NAME","khadafi_shop");
 
 $connection = mysqli_connect(HOST_NAME, USER_NAME, PASSWORD, DB_NAME);
